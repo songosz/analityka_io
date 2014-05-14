7b2ffe6574b015a0bbdec74d1fc6b9c882510234
Grzegorz <malpa@malpa.pl>
2014-05-14 15:46:14 +0200

    dodana funkcja getinfor

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,10 @@
+def getinfo():
+    print("Program kalkulator)
+
 def dodawanie():
     a=int(input('Wprowadz pierwza liczbe'))
     b=int(input('Wprowadz druga liczbe'))
     print(a+b)
     
+getinfo()
 dodawanie()
